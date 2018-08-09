df7ec6f002a8544107c333b7c48a306d26852cd4
lac417 <lac417@gmail.com>
2018-08-09 19:14:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,11 @@ class Dog
     @this_dogs_name = dog_name
   end
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
 lassie = Dog.new
 lassie.name = "Lassie"
 
-lassie.name
+puts lassie.name
